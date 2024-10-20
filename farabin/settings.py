9a1a4b767ba165343da3f45323e12957e313fc23
MADASSandD <ds1380ahmad@gmail.com>
2024-10-20 22:04:46 +0330

    Make migrations

--- farabin/settings.py
+++ farabin/settings.py
@@ -93,13 +93,23 @@ WSGI_APPLICATION = 'farabin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'farabin',
+        'USER': 'root',
+        'PASSWORD': 'R))TUS#R',
+        'HOST': 'localhost',
+        'PORT': '',  # Set to empty string for default
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
