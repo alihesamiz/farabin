fb04d4d8df64f1a8147991bf478048deb9f2e7c3
MADASSandD <ds1380ahmad@gmail.com>
2024-10-24 11:14:15 +0330

    Update files structures

--- diagnostics/models.py
+++ diagnostics/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.db import models
-from .utils import CustomUtils
 
 
 ####################################
