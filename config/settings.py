9889883a7e1b658305e860bd18d34e57e07dc443
MADASSandD <ds1380ahmad@gmail.com>
2024-11-17 21:34:21 +0330

    Update docker compose fiel configurations

--- config/settings.py
+++ config/settings.py
@@ -219,8 +219,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://127.0.0.1:6379",
-    "http://localhost:6379",
+    "http://redis:6379",
+    "http://redis:6379",
 ]
 
 
