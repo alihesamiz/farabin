9f7a7518dc1222cacb6ec34b9a51a70f296a9ad9
ahMAD ASSadi <ds1380ahmad@gmail.com>
2024-12-14 09:50:31 +0330

    Update run celery script

--- run_celery.sh
+++ run_celery.sh
@@ -1,4 +1,6 @@
-sudo apt install redis-server
-sudo systemctl restart redis-server
-sudo systemctl reload-daemon redis-server
-celery -A config worker --beat -l INFO
+#!/bin/bash
+# apt install redis-server
+# systemctl restart redis-server
+# systemctl reload-daemon redis-server
+celery -A config worker -l INFO
+celery -A config beat -l INFO
