8037cc4f08208453f43edcda5cee03a88f7e82d7
ahMAD ASSadi <ds1380ahmad@gmail.com>
2024-12-24 09:25:39 +0330

    Update .sh files

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-celery -A config worker -l INFO
-celery -A config beat -l INFO
+# celery -A config worker -l INFO
+# celery -A config beat -l INFO
+celery -A config worker --beat -l INFO
