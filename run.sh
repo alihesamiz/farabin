41fb98d9db9ee53cdb68b1ec9277322f2a25d508
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-01-15 12:01:24 +0330

    Update run.sh script for loading the .env file into the terminal session

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # Install Supervisor
-sudo apt install -y supervisor
+# sudo apt install -y supervisor
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Check if .env exists in the script directory
+if [ -f "$SCRIPT_DIR/.env" ]; then
+    echo "Loading environment variables from .env file in $SCRIPT_DIR..."
+    export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
+    echo "Done Loading..."
+else
+    echo "No .env file found in $SCRIPT_DIR."
+    exit 1
+fi
 
 # Shutdown any existing Supervisor instance (if running)
 supervisorctl -c supervisord.conf stop all
