90c1cc411094f582a075ad7d1b0ed8e3fd629e51
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-04 15:20:14 +0330

    ✨ Update management app
    views, admin, serializers and urls of the management app updated

--- management/urls.py
+++ management/urls.py
@@ -2,13 +2,17 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from management.views import HumanResourceViewSet
+from management.views import HumanResourceViewSet, OrganizationChartFileViewSet,PersonelInformationViewSet
 
 
-router = DefaultRouter()
+hr_router = DefaultRouter()
+hr_router.register(r'human-resources', HumanResourceViewSet, basename='human-resources')
 
-router.register(r'human-resource', HumanResourceViewSet, basename='human-resource')
+personel_router = DefaultRouter()
+personel_router.register(r'personel', PersonelInformationViewSet, basename='human-resource-personel')
 
+chart_file_router = DefaultRouter()
+chart_file_router.register(r'organization-chart-files', OrganizationChartFileViewSet, basename='organization-chart-files')
 
+urlpatterns = hr_router.urls + personel_router.urls+chart_file_router.urls
 
-urlpatterns = router.urls
--- management/views.py
+++ management/views.py
@@ -2,11 +2,12 @@ from django.db import IntegrityError
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
-from rest_framework import viewsets,status
+from rest_framework import serializers
+from rest_framework import viewsets
 
-from management.serializers import HumanResourceSerializer, HumanResourceCreateSerializer, HumanResourceUpdateSerializer
-from management.models import HumanResource
+from management.serializers import (HumanResourceSerializer, HumanResourceCreateSerializer, HumanResourceUpdateSerializer,
+                                    PersonelInformationSerializer, PersonelInformationUpdateSerializer, PersonelInformationCreateSerializer, OrganizationChartFileSerializer)
+from management.models import HumanResource, PersonelInformation, OrganizationChartBase
 
 # Create your views here.
 
@@ -24,9 +25,46 @@ class HumanResourceViewSet(viewsets.ModelViewSet):
         elif self.action in ['update', 'partial_update']:
             return HumanResourceUpdateSerializer
         return HumanResourceSerializer
-    
+
     def perform_create(self, serializer):
-            try:
-                serializer.save()
-            except IntegrityError:
-                raise ValidationError({"error": "Each company can only have one Human Resource record."})
+        try:
+            serializer.save()
+        except IntegrityError:
+            raise ValidationError(
+                {"error": "Each company can only have one Human Resource record."})
+
+
+class PersonelInformationViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        company = self.request.user.company
+        return PersonelInformation.objects.select_related("human_resource").filter(human_resource__company=company)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return PersonelInformationCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return PersonelInformationUpdateSerializer
+        return PersonelInformationSerializer
+
+    def perform_create(self, serializer):
+        company = self.request.user.company
+        human_resource = company.hrfiles.order_by(
+            "-create_at").first()  # Ensure latest HR file is used
+        if not human_resource:
+            raise serializers.ValidationError(
+                {"human_resource": "No HumanResource record found for this company."})
+        serializer.save(human_resource=human_resource)
+
+
+class OrganizationChartFileViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = OrganizationChartFileSerializer
+
+    http_method_names = ['get']
+    def get_queryset(self):
+        company = self.request.user.company
+        kind = company.tech_field
+        return OrganizationChartBase.objects.filter(field=kind)
