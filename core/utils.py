881d40a73136e35074e7bb0d22e3d34b1307d447
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-04 11:08:07 +0330

    ♻️ Refactore and update catching mechanism

--- core/utils.py
+++ core/utils.py
@@ -1,29 +1,33 @@
-from django.core.files.storage import default_storage
-from django.utils.translation import gettext_lazy as _
-from django.utils.deconstruct import deconstructible
-from django.db import transaction
-from django.db import models
-from math import log2
-from core.models import User
 import numpy as np
 import requests
 import json
 import re
 import os
 
+from django.core.files.storage import default_storage
+from django.utils.translation import gettext_lazy as _
+from django.utils.deconstruct import deconstructible
+from django.db import transaction
+from django.db import models
+
+from core.models import User
+
+
 
 @deconstructible
 class GeneralUtils:
 
-    # SMS PROVIDER API
-    url = "https://api2.ippanel.com/api/v1/sms/pattern/normal/send"
+    
+    SMS_URL = "https://api2.ippanel.com/api/v1/sms/pattern/normal/send"
     
     GREEN = "\033[92m"
     
     RESET = "\033[0m"
 
-    headers = {
-        'apikey': 'BXb2ovSeYtiAbfVT26gEb50Dmix_-nhAAQRp2v5yfXs=',
+    EXCEPTION_MODELS=["OrganizationChart"]
+    
+    SMS_AUTH_HEADER = {
+        'apikey': os.getenv('FARABIN_SMS_API_KEY','BXb2ovSeYtiAbfVT26gEb50Dmix_-nhAAQRp2v5yfXs='),
         'Content-Type': 'application/json'
     }
 
@@ -31,19 +35,16 @@ class GeneralUtils:
         self.path = path
         self.fields = fields
 
-    # Generate a 6-digit OTP
-
     def generate_otp(self):
         """Generate a 6-digit OTP."""
         return str(np.random.randint(100000, 999999))
 
-    # Send OTP SMS using the SMS provider API
     def send_otp(self, phone_number, otp):
         """Send OTP to the given phone number (placeholder for SMS integration)."""
 
         with transaction.atomic():
             payload = json.dumps({
-                "code": "0vdhr2n9d5b2j78",
+                "code": os.getenv("FARABIN_OTP_PATTERN","0vdhr2n9d5b2j78"),
                 "sender": "+983000505",
                 "recipient": phone_number,
                 "variable": {
@@ -52,19 +53,19 @@ class GeneralUtils:
             })
 
             response = requests.request(
-                "POST", self.url, headers=self.headers, data=payload)
+                "POST", self.SMS_URL, headers=self.SMS_AUTH_HEADER, data=payload)
 
         print(f"{self.GREEN}Sending OTP {otp} to {phone_number}")
         
         return response.text
 
-    # SLUGIFY a string, keeping Persian characters and numbers.
     def persian_slugify(self, value: str):
         """Slugify a string, keeping Persian characters and numbers."""
-        # Remove extra spaces
+
         value = value.strip()
-        # Replace spaces with hyphens
+
         value = re.sub(r'[\s]+', '-', value)
+
         return value.strip('-')
 
     def rename_folder(self, instance, filename: str):
@@ -72,44 +73,49 @@ class GeneralUtils:
         Dynamically rename the file based on the company's name, year, and the field name.
         If the company's name has changed, rename the folder structure accordingly.
         """
-        # Get the file extension
+        
         ext = filename.split('.')[-1].lower()
 
-        # Collect the field values for filename generation (company name and year)
         field_values = []
         for field in self.fields:
+        
             attrs = field.split('__')
             value = instance
+        
             try:
                 for attr in attrs:
                     value = getattr(value, attr)
+        
             except AttributeError:
                 value = ''
                 break
+        
             field_values.append(str(value))
 
-        # Slugify the field values (company name and year)
         base_filename = self.persian_slugify("-".join(field_values))
 
-        # Ensure the filename is not empty
         if not base_filename:
             base_filename = 'file'
             
-        try:
-            if hasattr(instance.company, 'id'):
-                current_slug = instance.company.id
-        except:
-            current_slug = instance.issuer.id if hasattr(
-                instance.issuer, 'id') else 'default-company'
-        # current_slug = self.persian_slugify(current_company_title)
+        if instance._meta.model.__name__ not in self.EXCEPTION_MODELS:
+            try:
+                if hasattr(instance.company, 'id'):
+                    current_slug = instance.company.id
+
+            except:
+                current_slug = instance.issuer.id if hasattr(
+                    instance.issuer, 'id') else 'default-company'
+
+        else:
+            current_slug = 'organization_excel_files'
+
         year = getattr(instance, 'year', '')
         month = getattr(instance, 'month', '')
-
-        # Determine the existing folder structure (in case the company name has changed)
+        
         previous_slug = getattr(
             instance, '_previous_company_slug', current_slug)
 
-        # Check if the company name has changed
+        
         if previous_slug != current_slug:
             old_folder_path = f"{self.path}/{previous_slug}/{year}"
             new_folder_path = f"{self.path}/{current_slug}/{year}"
@@ -117,19 +123,19 @@ class GeneralUtils:
             if month:
                 old_folder_path += f"/{month}"
                 new_folder_path += f"/{month}"
-
-            # Rename the existing folder to the new path if it exists
+            
             if default_storage.exists(old_folder_path):
                 default_storage.move(old_folder_path, new_folder_path)
-
-        # Generate the full path for the file
+        
         field_name = 'unknown-field'
         for field in instance._meta.fields:
+            
             if isinstance(field, models.FileField):
                 file_field = getattr(instance, field.name, None)
                 if file_field and hasattr(file_field, 'name') and file_field.name == filename:
                     field_name = field.name
                     break
+        
         if year != "":
             if month:
                 final_path = f"{
@@ -137,10 +143,11 @@ class GeneralUtils:
             else:
                 final_path = f"{
                     self.path}/{current_slug}/{year}/{base_filename}-{field_name}.{ext}"
+        
         else:
             final_path = f"{
                 self.path}/{current_slug}/{base_filename}-{field_name}.{ext}"
-        # Store the current slug as the '_previous_company_slug' for future checks
+        
         setattr(instance, '_previous_company_slug', current_slug)
 
         return final_path
@@ -151,6 +158,7 @@ class GeneralUtils:
             groups=1).values_list('phone_number', flat=True)
 
         with transaction.atomic():
+        
             payload = json.dumps({
                 "code": "0vdhr2n9d5b2j78",
                 "sender": "+983000505",
@@ -161,20 +169,16 @@ class GeneralUtils:
             })
 
             response = requests.request(
-                "POST", self.url, headers=self.headers, data=payload)
+                "POST", self.SMS_URL, headers=self.SMS_AUTH_HEADER, data=payload)
         
         
         print(f"Sending {message}to {editors}")
 
         return response.text
 
-        # TODO: Implement the SMS sending logic here using the SMS provider API.
-        pass
-
     def deconstruct(self):
-        # This will tell Django to consider this field unchanged in migrations
         return (
             'core.GeneralUtils',
-            [self.path, self.fields],  # arguments
-            {}  # keyword arguments
+            [self.path, self.fields],
+            {}
         )
