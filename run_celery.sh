34279d4b0770c9743df9fedda647d3b720b9176e
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-05 11:28:55 +0330

    ♻️ Update executable scripts
    start, stop, status and run_celery scripts updated for a better perfrmance and error handling

--- run_celery.sh
+++ run_celery.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-# celery -A config worker -l INFO
-# celery -A config beat -l INFO
+#! /bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ -f "$SCRIPT_DIR/.env" ]; then
+    echo "Loading environment variables from .env file in $SCRIPT_DIR..."
+    export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
+    echo "Done Loading..."
+else
+    echo "No .env file found in $SCRIPT_DIR."
+    exit 1
+fi
+
 celery -A config worker --beat -l INFO
