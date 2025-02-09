236cbc5f402a855666a065bb0b7bd56a8aa00e0c
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-09 14:54:09 +0330

    ✨ Prepare excel task
    async prepare-excel function for renaming the columns

--- management/tasks.py
+++ management/tasks.py
@@ -1,16 +1,32 @@
-from pathlib import Path
-import pandas as pd
+import os
 
-from django.db.transaction import atomic
+from openpyxl import load_workbook
+
+from django.conf import settings
 
 from celery import shared_task
 
 
-@shared_task(bind=True)
-def process_excel(file_path:Path):
-    df = pd.read_excel(file_path,usecols="B:E",skiprows=2,engine="openpyxl")
-    
-    for _,row in df.iterrows():
-        with atomic():
-            print("lkasjdlksajdklsajd")
-        
+@shared_task()
+def prepare_excel(company_name: str, file_path: str, action: str = "rename-column"):
+    if action == "rename-column":
+        try:
+            wb = load_workbook(file_path)
+            ws = wb.active
+
+            for col in ws.iter_cols(min_col=3, max_col=3, max_row=1):
+                for cell in col:
+                    cell.value = cell.value.replace('x', company_name)
+
+            new_filename = f"updated_{company_name}.xlsx"
+            updated_file_path = os.path.join(
+                settings.MEDIA_ROOT, "organization_charts", new_filename)
+
+            os.makedirs(os.path.dirname(updated_file_path), exist_ok=True)
+
+            wb.save(updated_file_path)
+
+            return updated_file_path
+
+        except Exception as e:
+            return str(e)
