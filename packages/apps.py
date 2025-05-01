532e69fe22d41e345fc2c11810be3c34fd8419ac
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-01 15:47:23 +0330

    ✨ Update PackagesConfig to include verbose_name and ready method for signal registration

--- packages/apps.py
+++ packages/apps.py
@@ -1,6 +1,11 @@
+from django.utils.translation import gettext_lazy as _
 from django.apps import AppConfig
 
 
 class PackagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'packages'
+    verbose_name = _("Packages")
+
+    def ready(self):
+        from packages import signals
