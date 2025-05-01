16638ea8e466e667dfaa38f42dcd20b360530651
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-01 15:46:33 +0330

    ✨ Add Subscription and Order serializers with related fields and methods

--- packages/serializers.py
+++ packages/serializers.py
@@ -1,19 +1,70 @@
-from packages.models import Package, Service
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField, PrimaryKeyRelatedField, SerializerMethodField
 
-from rest_framework.serializers import ModelSerializer, SerializerMethodField, Serializer
-
+from packages.models import Order, Package, Service, Subscription
 
 class ServiceSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='services-detail', read_only=True)
+
     class Meta:
         model = Service
-        fields = ("id", "name", "description", "is_active")
-        read_only_fields = ("id",)
+        fields = ("id", "url", "name", "description", "is_active")
+        read_only_fields = ("id", "is_active",)
+
 
 class PackageSerializer(ModelSerializer):
     services = ServiceSerializer(many=True, read_only=True)
 
     class Meta:
         model = Package
-        fields = ("id", "name", "description", "price", "period", "is_active", "services")
-        read_only_fields = ("id",)
+        fields = ("id", "name", "description", "price",
+                  "period", "is_active", "services")
+        read_only_fields = ("id", "is_active",)
 
+
+class SubscriptionSerializer(ModelSerializer):
+    package = PackageSerializer(read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = [
+            "id",
+            "package",
+            "purchase_date",
+            "expires_at",
+            "duration",
+        ]
+
+
+class OrderSerializer(ModelSerializer):
+    package = PackageSerializer(read_only=True)
+    total_price = SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "user",
+            "status",
+            "package",
+            "total_price",
+            "created_at"
+        ]
+        read_only_fields = ["status", "created_at", "package", "user"]
+
+    def get_total_price(self, obj):
+        return obj.package.price
+
+
+class OrderCreateSerializer(ModelSerializer):
+    package_id = PrimaryKeyRelatedField(
+        queryset=Package.objects.all(), source="package"
+    )
+
+    class Meta:
+        model = Order
+        fields = ["id", "package_id"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        validated_data["user"] = user
+        return Order.objects.create(**validated_data)
