59d2534a4c03fc16db62b714831e342b6934647a
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-01 15:46:57 +0330

    ✨ Register Order model in admin and enhance PackageAdmin with filter_horizontal for services

--- packages/admin.py
+++ packages/admin.py
@@ -1,7 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 
-from packages.models import Service, Package, Subscription
+from packages.models import Order, Service, Package, Subscription
 
 
 @admin.register(Service)
@@ -24,6 +24,7 @@ class PackageAdmin(admin.ModelAdmin):
     list_filter = ("is_active",)
     search_fields = ("name", "description")
     ordering = ("-is_active", "-price")
+    filter_horizontal = ["services"]
 
 
 @admin.register(Subscription)
@@ -35,6 +36,24 @@ class SubscriptionAdmin(admin.ModelAdmin):
         "expires_at",)
     search_fields = ("user__phone_number", "package__name")
     ordering = ("-purchase_date",)
+
+    def company_title(self, obj):
+        return obj.user.company.company_title
+    company_title.short_description = _("Company Title")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "company_title",
+        "package",
+        "status",
+        "created_at",
+    ]
+    search_fields = ("user__phone_number",
+                     "user__company_company_title", "package__name", "status")
+    ordering = ("-created_at",)
+    list_filter = ("status", "package__name")
 
     def company_title(self, obj):
         return obj.user.company.company_title
