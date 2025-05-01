1b99d2cef553c7ead383d0b007305e54a335be90
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-01 15:48:42 +0330

    ✨ Update URL routing to include Order and Subscription viewsets

--- packages/urls.py
+++ packages/urls.py
@@ -1,11 +1,13 @@
-from rest_framework_nested.routers import NestedDefaultRouter
 from rest_framework.routers import DefaultRouter
 
-from packages.views import PackageViewSet, ServiceViewSet
+from packages.views import OrderViewSet, PackageViewSet, ServiceViewSet, SubscriptionsViewSet
 
 router = DefaultRouter()
-router.register(r"", PackageViewSet, basename="packages")
+router.register(r"list", PackageViewSet, basename="packages")
 router.register(r"services", ServiceViewSet, basename="services")
+router.register(r"subscription", SubscriptionsViewSet,
+                basename="subscriptions")
+router.register(r"order", OrderViewSet, basename="orders")
 
 
 urlpatterns = router.urls
