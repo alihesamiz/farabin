b335fc248d4e53b890f222af3600ac5a10325eca
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-05-08 18:59:08 +0330

    ✅ Add test configurations
    pytest.ini and test 'utils' created

---
+++ tests/utils.py
@@ -0,0 +1,30 @@
+import pytest
+
+from rest_framework.test import APIClient
+
+from core.models import OTP, User
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def create_user():
+    def _create_user(phone_number, national_code):
+        user = User.objects.create(
+            phone_number=phone_number, national_code=national_code)
+        return user
+    return _create_user
+
+
+@pytest.fixture
+def generate_otp():
+    def _generate_otp(user):
+        otp = OTP(user=user)
+        otp_code = otp.generate_otp()
+        otp.otp_code = otp_code
+        otp.save()
+        return otp_code
+    return _generate_otp
