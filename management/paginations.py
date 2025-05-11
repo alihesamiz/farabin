8040697c79306e87d7e1659b0a1fce9bfc249f76
ahMADASSadi <ds1380ahmad@gmail.com>
2025-05-11 15:46:28 +0330

    ♻️ Refactor pagination classes: extend BasePageNumberPagination for consistency and clarity

--- management/paginations.py
+++ management/paginations.py
@@ -1,7 +1,19 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class PersonelPagination(PageNumberPagination):
+class BasePageNumberPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
+
+
+class PersonelPagination(BasePageNumberPagination):
+    ...
+
+
+class SWOTQuestionPagination(BasePageNumberPagination):
+    ...
+
+
+class SWOTOptionPagination(BasePageNumberPagination):
+    ...
