fb6c6789f438271acb2c2721cdb90c82f3632f44
ahMADASSadi <ds1380ahmad@gmail.com>
2025-05-11 15:47:15 +0330

    ♻️ Refactor URL routing: streamline SWOTS options registration and improve organization of nested routers

--- management/urls.py
+++ management/urls.py
@@ -1,22 +1,24 @@
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
-from management.views import ChartNodeViewSet, HumanResourceViewSet, OrganizationChartFileViewSet, PersonelInformationViewSet, SWOTStrengthOptionViewSet, SWOTWeaknessOptionViewSet, SWOTOppotunityOptionViewSet, SWOTThreatOptionViewSet, SWOTMatrixViewSet
+from management.views import ChartNodeViewSet, HumanResourceViewSet, OrganizationChartFileViewSet, PersonelInformationViewSet, SWOTMatrixViewSet, SWOTOptionViewSet, SWOTQuestionViewSet
 
 router = DefaultRouter()
 router.register(r'human-resources', HumanResourceViewSet,
                 basename='human-resources')
 router.register(r'organization-chart-files',
                 OrganizationChartFileViewSet, basename='organization-chart-files')
+router.register(r"swot", SWOTMatrixViewSet, basename="swot")
+router.register(r"swot/questions", SWOTQuestionViewSet,
+                basename="swot-questions")
+router.register(r"swot-options", SWOTOptionViewSet,
+                basename="swot-options")
 
-nested_router = NestedDefaultRouter(
+
+hr_nested_router = NestedDefaultRouter(
     router, r'human-resources', lookup='human_resource')
-nested_router.register(r'personel', PersonelInformationViewSet,
-                       basename='human-resource-personel')
-nested_router.register(r'graph', ChartNodeViewSet,
-                       basename='human-resource-graph')
-nested_router.register(r'swot', SWOTMatrixViewSet, basename='swot')
-nested_router.register(
-    r'strength', SWOTStrengthOptionViewSet, basename='swot-strength')
+hr_nested_router.register(r'personel', PersonelInformationViewSet,
+                          basename='human-resource-personel')
+hr_nested_router.register(r'graph', ChartNodeViewSet,
+                          basename='human-resource-graph')
 
-
-urlpatterns = router.urls + nested_router.urls
+urlpatterns = router.urls + hr_nested_router.urls
