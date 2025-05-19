f1c1b2a394d0284f87bfcc78c8d1736d3d56a533
ahMADASSadi <ds1380ahmad@gmail.com>
2025-05-19 09:50:05 +0330

    ✨ Enhance Order and Promotion Admin: Add coupon field to OrderAdmin, implement PromotionAdmin with actions for activating and deactivating promotions, and improve display methods for better usability.

--- packages/admin.py
+++ packages/admin.py
@@ -1,7 +1,8 @@
 from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 from django.contrib import admin
 
-from packages.models import Order, Service, Package, Subscription
+from packages.models import Order, Promotion, Service, Package, Subscription
 
 
 @admin.register(Service)
@@ -68,7 +69,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
         return ", ".join(s.__str__() for s in obj.service.all())
 
     @admin.display(description=_("Company Title"))
-    def company_title(self, obj):
+    def company_title(self, obj: Subscription):
         return obj.user.company.company_title
 
 
@@ -79,8 +80,10 @@ class OrderAdmin(admin.ModelAdmin):
         "package",
         "services",
         "status",
-        "created_at",
+        "coupon",
         "price",
+        "total_price",
+        "created_at",
     ]
     search_fields = (
         "user__phone_number",
@@ -100,7 +103,7 @@ class OrderAdmin(admin.ModelAdmin):
     filter_horizontal = ["service"]
 
     @admin.display(description=_("Company Title"))
-    def company_title(self, obj):
+    def company_title(self, obj: Order):
         return obj.user.company.company_title
 
     @admin.display(description=_("Services"))
@@ -113,3 +116,51 @@ class OrderAdmin(admin.ModelAdmin):
             return obj.package.price
         elif obj.service.all():
             return sum(s.price for s in obj.service.all())
+
+    @admin.display(description=_("قیمت کلی"))
+    def total_price(self, obj: Order):
+        return obj.total_price
+
+
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display = ["coupon", "discount",
+                    "validated_from",
+                    "validated_until",
+                    "duration",
+                    "available_for",
+                    "is_active",
+                    "is_available",
+                    ]
+
+    actions = ["deactivate", "activate"]
+
+    @admin.display(description=_("قابل دسترس است"), boolean=True)
+    def is_available(self, obj: Promotion):
+        return obj.is_available
+
+    @admin.display(description=_("بازه زمانی"))
+    def duration(self, obj: Promotion):
+        return obj.duration if obj.duration != float("inf") else "بدون زمان"
+
+    @admin.action(description=_("غیرفعال کردن تخفیف"))
+    def deactivate(self, request, queryset: Promotion):
+        updated_count = queryset.update(is_active=False)
+        self.message_user(
+            request,
+            _("{}کد تخفیف غیرفعال شد(ند).").format(
+                updated_count
+            ),
+            messages.SUCCESS,
+        )
+
+    @admin.action(description=_("فعال کردن تخفیف"))
+    def activate(self, request, queryset: Promotion):
+        updated_count = queryset.update(is_active=True)
+        self.message_user(
+            request,
+            _("{}کد تخفیف فعال شد(ند).").format(
+                updated_count
+            ),
+            messages.SUCCESS,
+        )
