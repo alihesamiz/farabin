6eb4d17e71acb01372b3fc1ab6829f8fc6bf318a
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-17 08:22:05 +0330

    ♻️ Refactor UserAdmin in admin.py: Update fieldsets and search fields; enhance user management interface

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,12 +1,11 @@
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import get_user_model
-from django.contrib import admin
+from django.contrib import admin  # type: ignore
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _  # type: ignore
 
-
-from apps.core.models import PackagePermission, QuestionMetric, Service, OTP, UserPermission, Question, QuestionChoice, Questionnaire, QuestionnaireQuestion
-
-
-User = get_user_model()
+from apps.core.models import (
+    OTP,
+    User,
+)
 
 
 @admin.register(OTP)
@@ -15,22 +14,32 @@ class OtpAdmin(admin.ModelAdmin):
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         "phone_number",
-        "national_code",
+        "social_code",
         "is_active",
         "is_staff",
         "is_superuser",
     )
 
-    search_fields = ("phone_number", "national_code")
+    search_fields = ("phone_number", "social_code")
 
     ordering = ("phone_number",)
 
     fieldsets = (
-        (None, {"fields": ("phone_number", "password")}),
-        (_("Personal info"), {"fields": ("national_code",)}),
+        (
+            _("Personal info"),
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "social_code",
+                    "phone_number",
+                    "password",
+                ]
+            },
+        ),
         (
             _("Permissions"),
             {
@@ -53,7 +62,7 @@ class UserAdmin(admin.ModelAdmin):
                 "classes": ("wide",),
                 "fields": (
                     "phone_number",
-                    "national_code",
+                    "social_code",
                     "password1",
                     "password2",
                     "is_active",
@@ -72,62 +81,3 @@ class UserAdmin(admin.ModelAdmin):
 
     list_filter = ("is_active", "is_staff", "is_superuser")
 
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ["name", "description", "get_price", "service_active"]
-
-    def get_price(self, service: Service):
-        return f"{service.price:,.2f}"
-
-    get_price.short_description = _("Price")
-
-
-@admin.register(PackagePermission)
-class PackagePermissionAdmin(admin.ModelAdmin):
-    list_display = ("name", "codename", "description")
-    search_fields = ("name", "codename")
-
-
-@admin.register(UserPermission)
-class UserPermissionAdmin(admin.ModelAdmin):
-    list_display = ("user", "permission")
-    list_filter = ("permission",)
-    search_fields = ("user__username", "permission__name")
-
-
-class QuestionChoiceInline(admin.TabularInline):
-    model = QuestionChoice
-    extra = 0
-
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("text", "created_at")
-    search_fields = ("text",)
-    inlines = [QuestionChoiceInline]
-
-
-class QuestionnaireQuestionInline(admin.TabularInline):
-    model = QuestionnaireQuestion
-    extra = 1
-    autocomplete_fields = ['question']
-
-
-@admin.register(Questionnaire)
-class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_at", "updated_at")
-    search_fields = ("name",)
-    inlines = [QuestionnaireQuestionInline]
-
-
-@admin.register(QuestionChoice)
-class QuestionChoiceAdmin(admin.ModelAdmin):
-    list_display = ("question", "answer", "points")
-    list_filter = ("question",)
-    search_fields = ("answer",)
-
-
-@admin.register(QuestionMetric)
-class QuestionMetricAdmin(admin.ModelAdmin):
-    list_display = ["question", "title", "created_at"]
