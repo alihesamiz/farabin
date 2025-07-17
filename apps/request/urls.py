4393975942de67702cd6f7737b3776f506a81d25
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-17 09:16:48 +0330

    🔥 Remove all the migrations and changed(useless) scripts
    'cause of the version changing the migrations deleted; useless files due to structural changes deleted

--- apps/request/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from apps.request.views import RequestViewSet
-
-router = DefaultRouter()
-
-router.register(r"", RequestViewSet, basename="requests")
-
-urlpatterns = router.urls
