71b99870fa6a0127615fc917d29932f85559078d
ahMADASSadi <ds1380ahmad@gmail.com>
2025-07-17 08:27:41 +0330

    ♻️ Refactor UserManager and User model: Update national_code to social_code; enhance user creation logic and add profile completeness check

--- apps/core/managers.py
+++ apps/core/managers.py
@@ -8,32 +8,24 @@ logger = logging.getLogger("core")
 
 
 class UserManager(BaseManager):
-    def create_user(self, phone_number, national_code=None, password=None, **kwargs):
+    def create_user(self, phone_number, social_code=None, password=None, **kwargs):
         if not phone_number:
             logger.error("User creation failed: phone number is missing")
             raise ValueError("The phone number must be set")
 
-        if not national_code:
+        if not social_code:
             logger.error("User creation failed: national code is missing")
             raise ValueError("The national code must be set")
 
-        user = self.model(
-            phone_number=phone_number, national_code=national_code, **kwargs
-        )
+        user = self.model(phone_number=phone_number, social_code=social_code, **kwargs)
 
-        if password:
-            user.set_password(password)
-            logger.info(f"Setting password for user with phone: {phone_number}")
-
-        else:
-            user.set_unusable_password()
-            logger.warning(f"User with phone {phone_number} has an unusable password")
+        user.set_password(password if password else phone_number)
 
         user.save(using=self._db)
         logger.info(f"User created successfully: {phone_number}")
         return user
 
-    def create_superuser(self, phone_number, national_code, password=None, **kwargs):
+    def create_superuser(self, phone_number, social_code, password=None, **kwargs):
         kwargs.setdefault("is_active", True)
         kwargs.setdefault("is_staff", True)
         kwargs.setdefault("is_superuser", True)
@@ -45,7 +37,7 @@ class UserManager(BaseManager):
         logger.info(f"Creating superuser with phone: {phone_number}")
         return self.create_user(
             phone_number=phone_number,
-            national_code=national_code,
+            social_code=social_code,
             password=password,
             **kwargs,
         )
--- apps/core/models.py
+++ apps/core/models.py
@@ -2,39 +2,49 @@ from datetime import timedelta
 import random
 
 
-from django.contrib.auth.models import (
+from django.contrib.auth.models import (  # type: ignore
     AbstractBaseUser as BaseUser,
     PermissionsMixin,
     Group,
 )
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
-from django.db import models
+from django.utils.translation import gettext_lazy as _  # type: ignore
+from django.utils import timezone  # type: ignore
+from django.db import models  # type: ignore
 
 
-from apps.core import Validator as _validator  # noqa: F401
+from constants.validators import Validator as _validator
 from apps.core.managers import UserManager
 
 
 class TimeStampedModel(models.Model):
-    created_at = models.DateTimeField(
-        verbose_name=_("تاریخ ایجاد"), auto_now_add=True)
-    updated_at = models.DateTimeField(
-        verbose_name=_("تاریخ بروزرسانی"), auto_now_add=True)
+    created_at = models.DateTimeField(verbose_name=_("Created At"), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_("Updated At"), auto_now_add=True)
 
     class Meta:
         abstract = True
 
 
 class User(BaseUser, PermissionsMixin):
+    first_name = models.CharField(
+        max_length=120,
+        verbose_name=_("First name"),
+        null=True,
+        blank=True,
+    )
+    last_name = models.CharField(
+        max_length=120,
+        verbose_name=_("Last name"),
+        null=True,
+        blank=True,
+    )
     phone_number = models.CharField(
         max_length=11,
         unique=True,
-        validators=[_validator.phone_number_model_regex_validator],
+        validators=[_validator.phone_number_model_regex_validator()],
         verbose_name=_("Phone Number"),
     )
-    national_code = models.CharField(
-        max_length=11, unique=True, verbose_name=_("National Code")
+    social_code = models.CharField(
+        max_length=10, unique=True, verbose_name=_("Social Code")
     )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -60,8 +70,8 @@ class User(BaseUser, PermissionsMixin):
         related_query_name="user",
     )
 
-    USERNAME_FIELD = "national_code"
-    REQUIRED_FIELDS = ["phone_number"]
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["social_code"]
 
     objects = UserManager()
 
@@ -88,15 +98,25 @@ class User(BaseUser, PermissionsMixin):
     def is_admin(self):
         return self.is_superuser
 
+    @property
+    def is_profile_complete(self):
+        required_fields = [
+            self.first_name,
+            self.last_name,
+            self.phone_number,
+            self.social_code,
+        ]
+        return all(required_fields)
+
     class Meta:
         verbose_name = _("User")
         verbose_name_plural = _("Users")
 
 
 class OTP(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="otps")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="otps")
     otp_code = models.CharField(max_length=6)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def is_valid(self):
@@ -128,14 +148,21 @@ class City(models.Model):
         return self.name
 
     class Meta:
-        unique_together = [["name", "province"]]
         verbose_name = _("City")
         verbose_name_plural = _("Cities")
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_city_per_province",
+                fields=[
+                    "name",
+                    "province",
+                ],
+            )
+        ]
 
 
 class Province(models.Model):
-    name = models.CharField(max_length=200, unique=True,
-                            verbose_name=_("Name"))
+    name = models.CharField(max_length=200, unique=True, verbose_name=_("Name"))
 
     def __str__(self) -> str:
         return self.name
@@ -145,123 +172,27 @@ class Province(models.Model):
         verbose_name_plural = _("Provinces")
 
 
-class Service(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_("Service Name"))
+# class Service(models.Model):
+#     name = models.CharField(
+#         max_length=120,
+#         verbose_name=_("Service Name"),
+#     )
+#     description = models.TextField(
+#         verbose_name=_("Service Description"),
+#     )
+#     price = models.DecimalField(
+#         decimal_places=2,
+#         max_digits=20,
+#         verbose_name=_("Price"),
+#     )
+#     service_active = models.BooleanField(
+#         default=False,
+#         verbose_name=_("Service Active"),
+#     )
 
-    description = models.TextField(verbose_name=_("Service Description"))
+#     def __str__(self) -> str:
+#         return self.name
 
-    price = models.DecimalField(
-        decimal_places=2, max_digits=20, verbose_name=_("Price")
-    )
-
-    service_active = models.BooleanField(
-        default=False, verbose_name=_("Service Active")
-    )
-
-    class Meta:
-        verbose_name = _("Service")
-        verbose_name_plural = _("Services")
-
-    def __str__(self) -> str:
-        return f"{self.name} › {self.description[:10]}"
-
-
-class PackagePermission(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    codename = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
-    service = models.ManyToManyField(
-        "packages.Service", blank=True, related_name="permissions"
-    )
-    package = models.ForeignKey(
-        "packages.Package",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="permissions",
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class UserPermission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    permission = models.ForeignKey(PackagePermission, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ("user", "permission")
-
-
-class QuestionMetric(TimeStampedModel):
-    question = models.ForeignKey(
-        "QuestionChoice", on_delete=models.CASCADE, related_name="metric", verbose_name=_("سوال"))
-    title = models.CharField(verbose_name=_("Title"), max_length=128)
-    weight = models.DecimalField(verbose_name=_(
-        "Weight"), max_digits=4, decimal_places=2)
-
-    class Meta:
-        verbose_name = _("شاخص سوال")
-        verbose_name_plural = _("شاخص‌های سوالات")
-
-    def __str__(self):
-        return f"{self.title!s}"
-
-
-class QuestionChoice(models.Model):
-    question = models.ForeignKey(
-        "Question", on_delete=models.CASCADE, related_name="choices", verbose_name=_("سوال")
-    )
-    answer = models.CharField(max_length=128, verbose_name=_("پاسخ"))
-    points = models.IntegerField(verbose_name=_("امتیاز"))
-
-    class Meta:
-        verbose_name = _("گزینه‌ سوال")
-        verbose_name_plural = _("گزینه‌های سوالات")
-
-    def __str__(self):
-        return f"{self.answer} ({self.points} امتیاز)"
-
-
-class Question(TimeStampedModel):
-    text = models.CharField(max_length=255, verbose_name=_("سوال"))
-
-    class Meta:
-        verbose_name = _("سوال")
-        verbose_name_plural = _("سوالات")
-
-    def __str__(self):
-        return f"{self.text!s}"
-
-
-class Questionnaire(TimeStampedModel):
-    name = models.CharField(max_length=255, verbose_name=_("عنوان"))
-    questions = models.ManyToManyField(
-        Question, through="QuestionnaireQuestion", verbose_name=_("سوالات")
-    )
-
-    class Meta:
-        verbose_name = _("پرسشنامه")
-        verbose_name_plural = _("پرسشنامه‌ها")
-
-    def __str__(self):
-        return f"{self.name!s}"
-
-
-class QuestionnaireQuestion(TimeStampedModel):
-    questionnaire = models.ForeignKey(
-        Questionnaire, on_delete=models.CASCADE, verbose_name=_("پرسشنامه"))
-    question = models.ForeignKey(
-        Question, on_delete=models.CASCADE, verbose_name=_("سوال"))
-    order = models.PositiveIntegerField(default=0, verbose_name=_("عدد ترتیب"))
-
-    class Meta:
-        verbose_name = _("سوال پرسشنامه")
-        verbose_name_plural = _("سوالات پرسشنامه‌ها")
-        constraints = [
-            models.UniqueConstraint(
-                fields=["questionnaire", "question"],
-                name="unique_questionnaire_question"
-            )
-        ]
-        ordering = ['order']
+#     class Meta:
+#         verbose_name = _("Service")
+#         verbose_name_plural = _("Services")
