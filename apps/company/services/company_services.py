b70eeaa7a6104c68f9b8a16b1c645a05d0d3165a
ahMADASSadi <ds1380ahmad@email.com>
2025-07-30 16:48:37 +0330

    feat(company): add functionality to create company profile and user as a new user signup

--- apps/company/services/company_services.py
+++ apps/company/services/company_services.py
@@ -1,4 +1,6 @@
-from apps.company.models import CompanyUser
+from uuid import uuid4
+
+from apps.company.models import CompanyProfile, CompanyUser
 from apps.company.repositories import CompanyRepository as _repo
 
 
@@ -19,7 +21,6 @@ class CompanyService:
         """
         phone_number = validated_data["phone_number"]
 
-        # Get or create user
         user, created = _repo.get_or_create_user(phone_number, defaults=validated_data)
 
         if not created:
@@ -27,3 +28,21 @@ class CompanyService:
 
         company_user = cls.create_company_user(user, company, role)
         return company_user
+
+    @classmethod
+    def create_profile_for_user(cls, user):
+        placeholder_title = f"Company-{uuid4().hex[:6]}"
+        placeholder_code = f"{uuid4().int % 10**11:011d}"
+
+        company = CompanyProfile.objects.create(
+            title=placeholder_title,
+            national_code=placeholder_code,
+            address="(To be updated)",
+            tech_field_id=1,
+            special_field_id=1,
+            province_id=1,
+            city_id=1,
+            insurance_list=1,
+        )
+        cls.create_company_user(user, company, CompanyUser.Role.MANAGER)
+        return company
