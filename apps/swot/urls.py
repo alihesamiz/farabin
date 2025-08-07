ea2b4c11c192c40f7a3016544473cd01bca0a834
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-08-07 23:53:35 +0330

    feat(swot): add swot url routes

--- apps/swot/urls.py
+++ apps/swot/urls.py
@@ -6,7 +6,13 @@ from apps.swot.views import (
     SWOTOptionViewSet,
     SWOTQuestionViewSet,
 )
-from apps.swot.views.swot import CompanySWOTQuestionViewSet
+from apps.swot.views.swot import (
+    CompanySWOTOptionAnalysisViewSet,
+    CompanySWOTOptionMatrixViweSet,
+    CompanySWOTQuestionAnalysisViewSet,
+    CompanySWOTQuestionMatrixViweSet,
+    CompanySWOTQuestionViewSet,
+)
 
 router = DefaultRouter()
 
@@ -14,14 +20,29 @@ router.register("options", SWOTOptionViewSet, basename="options")
 router.register("questions", SWOTQuestionViewSet, basename="questions")
 
 
-company_swot_router = DefaultRouter("company")
+company_swot_router = DefaultRouter()
+company_swot_router.register(
+    "questions/matrix/analysis",
+    CompanySWOTQuestionAnalysisViewSet,
+    basename="question-matrix-analysis",
+)
+company_swot_router.register(
+    "options/matrix/analysis",
+    CompanySWOTOptionAnalysisViewSet,
+    basename="option-matrix-analysis",
+)
+company_swot_router.register(
+    "questions/matrix", CompanySWOTQuestionMatrixViweSet, basename="question-matrix"
+)
+company_swot_router.register(
+    "options/matrix", CompanySWOTOptionMatrixViweSet, basename="option-matrix"
+)
 company_swot_router.register(
     "options", CompanySWOTOptionViewSet, basename="company-swot-options"
 )
 company_swot_router.register(
     "questions", CompanySWOTQuestionViewSet, basename="company-swot-questions"
 )
-
 urlpatterns = [
     path("company/", include(company_swot_router.urls)),
 ]
