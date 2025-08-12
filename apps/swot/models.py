fe916c850c691a2beb6e7ce18bafb750b20f6bf8
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-08-13 00:11:17 +0330

    feat(swot): add swot matrix and analysis models along with the new custom json field for swot use case

--- apps/swot/models.py
+++ apps/swot/models.py
@@ -1,7 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_lifecycle import AFTER_SAVE, LifecycleModelMixin, hook
 
 from apps.core.models import TimeStampedModel
+from apps.swot.tasks import generate_swot_analysis
 
 
 class ExternalFactors(models.TextChoices):
@@ -67,7 +70,46 @@ class SWOTOption(models.Model):
         ]
 
 
-class SWOTModelMatrix(TimeStampedModel):
+class SWOTJSONField(models.JSONField):
+    """Json custom field for swot properties
+
+    only the key value pairs with this format are allowed:
+    >>> key : "value"
+
+    which the keys are the IDs for the matrix property type and the values are the -
+    user values or the pre-existing selective values
+    """
+
+    def validate(self, value, model_instance):
+        super().validate(value, model_instance)
+        self._validate_swot_format(value)
+
+    def _validate_swot_format(self, value):
+        """Validate that JSON has integer keys and string values"""
+        if not isinstance(value, dict):
+            raise ValidationError(_("Field must be a dictionary object."))
+
+        for key, val in value.items():
+            if isinstance(key, str):
+                try:
+                    int(key)
+                except ValueError:
+                    raise ValidationError(
+                        _(
+                            "Keys must be integers or string representations of integers."
+                        )
+                    )
+            elif not isinstance(key, int):
+                raise ValidationError(
+                    _("Keys must be integers or string representations of integers.")
+                )
+
+            # Check value is string
+            if not isinstance(val, str):
+                raise ValidationError(_("Values must be strings."))
+
+
+class SWOTMatrix(LifecycleModelMixin, TimeStampedModel):
     class SWOTMatrixType(models.TextChoices):
         QUESTIONNAIRE = "q", _("Questionnaire")
         INFERENTIAL = "i", _("Inferential")
@@ -82,14 +124,41 @@ class SWOTModelMatrix(TimeStampedModel):
     matrix_type = models.CharField(
         max_length=15, verbose_name=_("Type"), choices=SWOTMatrixType.choices
     )
-    opportunity = models.JSONField(verbose_name=_("Opportunity"))
-    threat = models.JSONField(verbose_name=_("Threat"))
-    weakness = models.JSONField(verbose_name=_("Weakness"))
-    strength = models.JSONField(verbose_name=_("Strength"))
+
+    opportunity = SWOTJSONField(verbose_name=_("Opportunity"))
+    threat = SWOTJSONField(verbose_name=_("Threat"))
+    weakness = SWOTJSONField(verbose_name=_("Weakness"))
+    strength = SWOTJSONField(verbose_name=_("Strength"))
 
     def __str__(self):
         return self.company.title
 
+    @hook(AFTER_SAVE)
+    def start_analysis_generation(self):
+        generate_swot_analysis.delay(self.pk)
+
     class Meta:
         verbose_name = _("SWOT Matrix")
         verbose_name_plural = _("SWOT Matrices")
+
+
+class SWOTAnalysis(TimeStampedModel):
+    matrix = models.ForeignKey(
+        SWOTMatrix,
+        verbose_name=_("Matrix"),
+        on_delete=models.CASCADE,
+        related_name="analysis",
+    )
+
+    so = models.TextField(verbose_name=_("SO Analysis"))
+    st = models.TextField(verbose_name=_("ST Analysis"))
+    wo = models.TextField(verbose_name=_("WO Analysis"))
+    wt = models.TextField(verbose_name=_("WT Analysis"))
+
+    def __str__(self):
+        return f"{self.matrix.company.title}"
+
+    class Meta:
+        verbose_name = _("SWOT Analysis")
+        verbose_name_plural = _("SWOT Analysis")
+        ordering = ["-created_at"]
