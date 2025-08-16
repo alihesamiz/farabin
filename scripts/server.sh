7170713b8c0d52981b1dd5d78c925b13d3cf5734
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-08-16 20:04:44 +0330

    fix(core): comment out the load_services and load_departments commands

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,11 +6,11 @@ WORKERS=$(python -c "import os; print(os.cpu_count())")
 FARABIN_PORT=$(python -c "import os; print(os.getenv('FARABIN_PORT', '8000'))")
 python manage.py migrate
 
-python manage.py load_departments
+# python manage.py load_departments
 python manage.py load_cities
 python manage.py load_special_fields
 python manage.py load_tech_fields
-python manage.py load_services
+# python manage.py load_services
 python manage.py load_excel_files
 python manage.py load_licenses
 python manage.py load_life_cycles
