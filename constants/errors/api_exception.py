f83674dfae6ea919b82e24d11a6fee319f5c4de3
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-08-18 02:40:20 +0330

    feat(constants): add new errors

--- constants/errors/api_exception.py
+++ constants/errors/api_exception.py
@@ -66,3 +66,39 @@ class NoCompanyAssignedError(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_detail = "No Company is assigned with the user"
     default_code = "no_company_assigned"
+
+
+class FinancialDataNotFoundError(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = "No Financial Data found for this period"
+    default_code = "no_financial_data"
+
+
+class FinancialChartNameError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "No chart configuration found for this chart name"
+    default_code = "no_chart_name"
+
+
+class FileYearAlreadyExists(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = "A file with this year already exists"
+    default_code = "file_exists_duplicate"
+
+
+class DeletionPermissionDenied(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = "You do not have permission to delete this file."
+    default_code = "permission_denied"
+
+
+class DatabaseSaveError(APIException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = "Could not save the changes to the database."
+    default_code = "database_error"
+
+
+class NoQueryParameterError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "No query parameter provided"
+    default_code = "no_query_parameter"
